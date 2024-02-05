f64bb28c7fd2545a5d4d5cfcf154e9e028e9ee7d
Nastia <adruts@mail.ru>
2024-02-05 21:25:26 +0300

    https://github.com/anastyadru/Zenject.git

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,18 @@
 using UnityEditor;
 using UnityEngine;
+using Zenject;
 
 public class GameController : MonoBehaviour
 {
     public bool CanMove { get; set; }
+    
+    [Inject]
+    private TimeController _timeController;
 
     public void Play()
     {
-        
+        // создать игрока и оппонентов
+        // создать финиш
     }
     public void Restart()
     {
---
+++ Assets/Scripts/TimeController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    public void SetPauseOn()
+    {
+        Time.timeScale = 0;
+    }
+    
+    public void SetPauseOff()
+    {
+        Time.timeScale = 1;
+    }
+}
