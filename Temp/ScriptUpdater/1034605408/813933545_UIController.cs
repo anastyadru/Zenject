6df94b331023ca45fd5d5c38e9a782e310da5629
Nastia <adruts@mail.ru>
2024-02-05 11:22:33 +0300

    https://github.com/anastyadru/Zenject.git

---
+++ Temp/ScriptUpdater/1034605408/813933545_UIController.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject _menuPanel;
+    [SerializeField] private GameObject _gamePanel;
+
+    void OnValidate()
+    {
+        _menuPanel = transform.Find("pnl_MainMenu").gameObject;
+        _menuPanel = transform.Find("pnl_GamePanel").gameObject;
+    }
+}
