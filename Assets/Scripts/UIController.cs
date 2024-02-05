033efecbe588a8b6cefe4e51d8eb1915cf9b633e
Nastia <adruts@mail.ru>
2024-02-05 11:44:00 +0300

    https://github.com/anastyadru/Zenject.git

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -10,4 +10,35 @@ public class UIController : MonoBehaviour
         _menuPanel = transform.Find("pnl_MainMenu").gameObject;
         _gamePanel = transform.Find("pnl_GamePanel").gameObject;
     }
+
+    public void ShowMenuPanel()
+    {
+        _menuPanel.SetActive(true);
+    }
+    public void HideMenuPanel()
+    {
+        _menuPanel.SetActive(false);
+    }
+    
+    public void ShowGamePanel()
+    {
+        _gamePanel.SetActive(true);
+    }
+    public void HideGamePanel()
+    {
+        _gamePanel.SetActive(false);
+    }
+
+    public void OnExitBtnClicked()
+    {
+        
+    }
+    public void OnPlayBtnClicked()
+    {
+        
+    }
+    public void OnRestartBtnClicked()
+    {
+        
+    }
 }
